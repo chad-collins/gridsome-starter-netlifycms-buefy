d3d7d76faede6a3d835453b1b639d63da18950a5
Chad Collins <chadcollins.co@gmail.com>
2020-07-29 18:35:46 -0400

    added filepaths for posts

--- gridsome.config.js
+++ gridsome.config.js
@@ -6,5 +6,58 @@
 
 module.exports = {
   siteName: 'Gridsome',
-  plugins: []
+  plugins: [
+
+    // Load Blog Posts
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: './content/blogposts/**/*.md',
+        typeName: 'BlogPost',
+        remark: {
+          plugins: ['@gridsome/remark-prismjs', { transformInlineCode: true }],
+        },
+      },
+    },
+
+    // Load Event Posts
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: './content/eventposts/**/*.md',
+        typeName: 'EventPost',
+        remark: {
+        
+        },
+      },
+    },
+    
+    // Netlify CMS
+
+    {
+      use: `gridsome-plugin-netlify-cms`,
+      options: {
+        publicPath: `/admin`,
+      },
+    },
+
+  ],
+
+  // Templates with Paths
+  templates: {
+    EventPost: '/event/:year/:month/:day/:title',
+    BlogPost: '/blog/:year/:month/:day/:title',
+  },
+
+  // Transformers
+  transformers: {
+    remark: {
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+      anchorClassName: 'icon icon-link',
+      plugins: [
+        // ...global plugins
+      ],
+    },
+  },
 }
